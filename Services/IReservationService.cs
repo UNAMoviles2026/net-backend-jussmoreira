557d450b0c5f928adb4ce9c5a5a3ea24135f74bc
Justin <jus111moreiramatarrita@gmail.com>
2026-03-30 16:28:57 -0600

    Add Service Layer
    Implement the service to contain the business logic.

---
+++ Services/IReservationService.cs
@@ -0,0 +1,10 @@
+using System.Threading.Tasks;
+using reservations_api.DTOs.Requests;
+using reservations_api.DTOs.Responses;
+
+namespace reservations_api.Services;
+
+public interface IReservationService
+{
+    Task<ReservationResponse> CreateAsync(CreateReservationRequest request);
+} 
